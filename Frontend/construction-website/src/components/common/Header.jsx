86bdaaec5cf98327ebe4fc8bfd0423b28ea07d90
Alishba shekh <alishbashabbir890@gmail.com>
2026-04-06 18:00:52 +0500

    project all set

--- Frontend/construction-website/src/components/common/Header.jsx
+++ Frontend/construction-website/src/components/common/Header.jsx
@@ -3,7 +3,7 @@ import { User, MenuIcon, BellIcon } from "lucide-react";
 
 const Header = ({ toggleSidebar, user }) => {
   return (
-    <header className="sticky top-0 z-40 w-full h-16 bg-white/80 backdrop-blur-xl border-b border-slate-200/60">
+    <header className="sticky top-0 z-40 w-full h-16 bg-white/80 backdrop-blur-xl border-b border-slate-200/60 shadow-md">
       
       <div className="flex items-center justify-between h-full px-6">
         
