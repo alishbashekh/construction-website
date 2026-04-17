3a06940e7962d14620144c3df2ac21f62758aa16
Engr. UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2026-04-17 16:09:17 +0500

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-     await mongoose.connect(process.env.MONGO_DB_URL);
-     console.log("mongodb is connected successfully");
-     
-    }catch (error){
-      console.error('database connection failed:', error.message);
-      process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URL);
+    console.log("✅ MongoDB is connected successfully");
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export const getVendorDB = () => {
+  return mongoose.connection;
 };
 
 export const disconnectDB = async () => {
   await mongoose.disconnect();
-  console.log("database disconnected");
-  
-}
+  console.log("📡 Database disconnected");
+};
 
 export default connectDB;
