40b40ce3b805036392b6049bcac72a64771ba96e
Engr. UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2026-04-23 11:37:54 +0500

    Delete Backend/Backend directory

--- Backend/Backend/middlewares/apiLogs.js
+++
@@ -1,8 +0,0 @@
-const apiLogs = (req, res, next) => {
-  const time = new Date().toLocaleString();
-  // Log the Method (GET/POST), URL, and Time
-  console.log(`[${time}] ${req.method} request to: ${req.url}`);
-  next();
-};
-
-export default apiLogs;
